95aebf8d117238043b9a6952f5d6b861d9a76fa7
DESKTOP-NRUK56G\DELL <kawidirangalla@gmail.com>
2024-10-30 14:18:15 +0530

    Create QuestionBank

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { Questions } from '../Helpers/QuestionBank';
 
 function Quiz() {
-  return (
-    <div>
-      Quiz
+    const [currQuestion, setCurrQuestion] = useState(0);
+    const [optionChosen, setOptuonChosen] = useState("")
+
+    return (
+    <div className='Quiz'>
+        <h1>{Questions[currQuestion].prompt}</h1>
+        <div className='options'>
+            <button onClick={() => setOptuonChosen("A")}> 
+                {Questions[currQuestion].optionA}{" "}
+            </button>
+            <button onClick={() => setOptuonChosen("B")}> 
+                {Questions[currQuestion].optionB}{" "}
+            </button>
+            <button onClick={() => setOptuonChosen("C")}> 
+                {Questions[currQuestion].optionC}{" "}
+            </button>
+            <button onClick={() => setOptuonChosen("D")}> 
+                {Questions[currQuestion].optionD}{" "}
+            </button>
+        </div>
+
+        <button>Next Question</button>
     </div>
   )
 }
---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt:"What is 1+3?",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer: "C",
+    },
+    {
+        prompt:"Which is a Streaming Platform?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Google",
+        optionD: "2Robinhood",
+        answer: "B",
+    },
+    {
+        prompt:"Favourite food?",
+        optionA: "Apple",
+        optionB: "Grapes",
+        optionC: "Banana",
+        optionD: "Orange",
+        answer: "A",
+    },
+];
